3ee629e5e9ced979fe2d062b34e0a1718673282c
Rafael Borri <86018276+leafarirrob@users.noreply.github.com>
2022-01-01 10:48:49 +0100

    Update python snake.py

--- python snake.py
+++ python snake.py
@@ -1,7 +1,4 @@
-Python 3.7.9 (v3.7.9:13c94747c7, Aug 15 2020, 01:31:08) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import pygame
+import pygame
 import random
 from enum import Enum
 from collections import namedtuple
